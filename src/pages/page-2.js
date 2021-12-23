47d7afbe39a8e2ca97ac75a45a567f5113a227df
1tjcaron <1tjcaron@gmail.com>
2021-12-23 08:18:43 -0500

    Update page-2.js
    update language

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,9 +6,9 @@ import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <Seo title="Oath Success" />
+    <h1>Hello!  You have successfully connected your credentials.</h1>
+    <p>We will now connect your data to Nool.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
